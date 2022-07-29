be5526c150113a654d2c92e8b3b01c1fdf93a272
Kyle VanTassell <octicon@gmail.com>
2022-07-29 13:59:41 -0500

    server up and running

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('server/public'));
 app.use(express.urlencoded());
 
-app.get();
+// app.get();
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
