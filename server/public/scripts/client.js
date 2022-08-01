46aa4a5884fd412057da5ace59b51bc83028f9d6
Kyle VanTassell <octicon@gmail.com>
2022-07-31 19:24:48 -0500

    operator click handlers / console logs

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,22 +2,44 @@ $(readyNow);
 
 function readyNow() {
     console.log('rn');
+    $('#add-butt').on('click', add);
+    $('#sub-butt').on('click', sub);
+    $('#mlt-butt').on('click', mlt);
+    $('#div-butt').on('click', div);
     $('#equal-butt').on('click', equalitator);
     $('#c-butt').on('click', clearFields);
 
-}
+};
+
+function add() {
+    console.log('add');
+};
+
+function sub() {
+    console.log('sub');
+};
+
+function mlt() {
+    console.log('mlt');
+};
+
+function div() {
+    console.log('div');
+};
 
 function equalitator() {
-    $.ajax({
-        type: 'POST',
-        url: '/inputs',
-        data: {
-            inputA: $('#input-a').val(),
-            operator: $(this).span,
-            inputB: $('#input-b').val(),
-        },
-    })
-}
+    console.log('eq');
+    // $.ajax({
+    //     type: 'POST',
+    //     url: '/inputs',
+    //     data: {
+    //         inputA: $('#input-a').val(),
+    //         operator: $(this).span,
+    //         inputB: $('#input-b').val(),
+    //     },
+    // })
+    
+};
 
 function clearFields() {
     $('input').val('');
