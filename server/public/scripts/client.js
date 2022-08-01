3e61d71b5b75b69f4e60f88cd84c2414fa0a2c15
Kyle VanTassell <octicon@gmail.com>
2022-08-01 18:17:36 -0500

    POST/GET showing up on DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,40 +8,74 @@ function readyNow() {
     $('#div-butt').on('click', div);
     $('#equal-butt').on('click', equalitator);
     $('#c-butt').on('click', clearFields);
+    getCalculations();
+    
 
 };
+let operator = '';
 
 function add() {
-    console.log('add');
+    // console.log('add');
+    operator = '+';
 };
 
 function sub() {
-    console.log('sub');
+    // console.log('sub');
+    operator = '-';
 };
 
 function mlt() {
-    console.log('mlt');
+    // console.log('mlt');
+    operator = '*';
 };
 
 function div() {
-    console.log('div');
+    // console.log('div');
+    operator = '/';
 };
 
 function equalitator() {
-    console.log('eq');
-    // $.ajax({
-    //     type: 'POST',
-    //     url: '/inputs',
-    //     data: {
-    //         inputA: $('#input-a').val(),
-    //         operator: $(this).span,
-    //         inputB: $('#input-b').val(),
-    //     },
-    // })
+    // console.log('eq');
+    addCalculations();
     
 };
 
 function clearFields() {
     $('input').val('');
-    console.log('cf');
+    // console.log('cf');
+}
+
+function addCalculations() {
+    // console.log('operator;  ', operator);
+    $.ajax({
+        type: 'POST',
+        url: '/inputs',
+        data: {
+            inputA: $('#input-a').val(),
+            operator: operator,
+            inputB: $('#input-b').val(),
+            
+        }
+    }).then(function (response){
+        console.log('added calculation:', response);
+        getCalculations();
+        clearFields();
+    })
+}
+
+function getCalculations(){
+    $.ajax({
+        type: 'GET',
+        url: '/inputs',
+    }).then(function (response) {
+        console.log(response);
+        $('#results').empty();
+        for (let i = 0; i < response.length; i++) {
+            let calc = response[i];
+            $('#results').append(`
+            <div>${calc.inputA} ${calc.operator} ${calc.inputB} ${`=`} ${calc.output}</div>
+            
+            `);
+        }
+    });
 }
--- server/server.js
+++ server/server.js
@@ -6,8 +6,35 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('server/public'));
 app.use(express.urlencoded());
 
-const calculations = [];
+let inputA;
+let operator;
+let inputB;
+let output;
 
+const calculationsList = [
+    {
+        inputA: 24,
+        operator: '+',
+        inputB: 19,
+        output: 43,
+        
+    }
+];
+
+// function doMath()
+
+// GET calculation inputs 
+app.get('/inputs', (req, res) => {
+    res.send(calculationsList);
+});
+
+// Post new calculations to the DOM
+app.post('/inputs', (req, res) => {
+    const calculation = req.body;
+    console.log(req.body);
+    calculationsList.push(calculation);
+    res.send(calculationsList);
+})
 
 
 app.listen(PORT, () => {
